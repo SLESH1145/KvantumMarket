8a678458c869d227a7bdbe77d8cf2f4475610e15
Tomek <MAGICSLESH@GMAIL.COM>
2022-03-30 19:49:11 +0300

    new shops

--- KvantumMarket/Kvantum Market/Form1.Designer.cs
+++ KvantumMarket/Kvantum Market/Form1.Designer.cs
@@ -32,13 +32,22 @@ namespace Kvantum_Market
             this.components = new System.ComponentModel.Container();
             this.search_string = new System.Windows.Forms.TextBox();
             this.start_search = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.inf_eld = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.img_eld = new System.Windows.Forms.PictureBox();
+            this.inf_mv = new System.Windows.Forms.TextBox();
+            this.img_mv = new System.Windows.Forms.PictureBox();
+            this.inf_el = new System.Windows.Forms.TextBox();
+            this.img_el = new System.Windows.Forms.PictureBox();
+            this.inf_ya = new System.Windows.Forms.TextBox();
+            this.img_ya = new System.Windows.Forms.PictureBox();
+            this.img_sb = new System.Windows.Forms.PictureBox();
+            this.inf_sb = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img_eld)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_mv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_el)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_ya)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_sb)).BeginInit();
             this.SuspendLayout();
             // 
             // search_string
@@ -58,48 +67,110 @@ namespace Kvantum_Market
             this.start_search.UseVisualStyleBackColor = true;
             this.start_search.Click += new System.EventHandler(this.start_search_Click);
             // 
-            // textBox1
+            // inf_eld
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.textBox1.Location = new System.Drawing.Point(12, 89);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(188, 172);
-            this.textBox1.TabIndex = 2;
+            this.inf_eld.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.inf_eld.Location = new System.Drawing.Point(12, 89);
+            this.inf_eld.Multiline = true;
+            this.inf_eld.Name = "inf_eld";
+            this.inf_eld.ReadOnly = true;
+            this.inf_eld.Size = new System.Drawing.Size(188, 172);
+            this.inf_eld.TabIndex = 2;
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // pictureBox1
+            // img_eld
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pictureBox1.Location = new System.Drawing.Point(197, 89);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(119, 172);
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.img_eld.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.img_eld.Location = new System.Drawing.Point(197, 89);
+            this.img_eld.Name = "img_eld";
+            this.img_eld.Size = new System.Drawing.Size(119, 172);
+            this.img_eld.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_eld.TabIndex = 3;
+            this.img_eld.TabStop = false;
             // 
-            // textBox2
+            // inf_mv
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.textBox2.Location = new System.Drawing.Point(378, 89);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(188, 172);
-            this.textBox2.TabIndex = 4;
+            this.inf_mv.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.inf_mv.Location = new System.Drawing.Point(378, 89);
+            this.inf_mv.Multiline = true;
+            this.inf_mv.Name = "inf_mv";
+            this.inf_mv.ReadOnly = true;
+            this.inf_mv.Size = new System.Drawing.Size(188, 172);
+            this.inf_mv.TabIndex = 4;
             // 
-            // pictureBox2
+            // img_mv
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pictureBox2.Location = new System.Drawing.Point(567, 89);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(119, 172);
-            this.pictureBox2.TabIndex = 5;
-            this.pictureBox2.TabStop = false;
+            this.img_mv.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.img_mv.Location = new System.Drawing.Point(567, 89);
+            this.img_mv.Name = "img_mv";
+            this.img_mv.Size = new System.Drawing.Size(119, 172);
+            this.img_mv.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_mv.TabIndex = 5;
+            this.img_mv.TabStop = false;
+            // 
+            // inf_el
+            // 
+            this.inf_el.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.inf_el.Location = new System.Drawing.Point(741, 89);
+            this.inf_el.Multiline = true;
+            this.inf_el.Name = "inf_el";
+            this.inf_el.ReadOnly = true;
+            this.inf_el.Size = new System.Drawing.Size(188, 172);
+            this.inf_el.TabIndex = 6;
+            // 
+            // img_el
+            // 
+            this.img_el.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.img_el.Location = new System.Drawing.Point(926, 89);
+            this.img_el.Name = "img_el";
+            this.img_el.Size = new System.Drawing.Size(119, 172);
+            this.img_el.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_el.TabIndex = 7;
+            this.img_el.TabStop = false;
+            // 
+            // inf_ya
+            // 
+            this.inf_ya.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.inf_ya.Location = new System.Drawing.Point(12, 308);
+            this.inf_ya.Multiline = true;
+            this.inf_ya.Name = "inf_ya";
+            this.inf_ya.ReadOnly = true;
+            this.inf_ya.Size = new System.Drawing.Size(188, 172);
+            this.inf_ya.TabIndex = 8;
+            // 
+            // img_ya
+            // 
+            this.img_ya.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.img_ya.Location = new System.Drawing.Point(197, 308);
+            this.img_ya.Name = "img_ya";
+            this.img_ya.Size = new System.Drawing.Size(119, 172);
+            this.img_ya.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_ya.TabIndex = 9;
+            this.img_ya.TabStop = false;
+            // 
+            // img_sb
+            // 
+            this.img_sb.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.img_sb.Location = new System.Drawing.Point(567, 308);
+            this.img_sb.Name = "img_sb";
+            this.img_sb.Size = new System.Drawing.Size(119, 172);
+            this.img_sb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.img_sb.TabIndex = 10;
+            this.img_sb.TabStop = false;
+            // 
+            // inf_sb
+            // 
+            this.inf_sb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.inf_sb.Location = new System.Drawing.Point(378, 308);
+            this.inf_sb.Multiline = true;
+            this.inf_sb.Name = "inf_sb";
+            this.inf_sb.ReadOnly = true;
+            this.inf_sb.Size = new System.Drawing.Size(188, 172);
+            this.inf_sb.TabIndex = 11;
             // 
             // KvantuMarket
             // 
@@ -107,17 +178,26 @@ namespace Kvantum_Market
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1071, 639);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inf_sb);
+            this.Controls.Add(this.img_sb);
+            this.Controls.Add(this.img_ya);
+            this.Controls.Add(this.inf_ya);
+            this.Controls.Add(this.img_el);
+            this.Controls.Add(this.inf_el);
+            this.Controls.Add(this.img_mv);
+            this.Controls.Add(this.inf_mv);
+            this.Controls.Add(this.img_eld);
+            this.Controls.Add(this.inf_eld);
             this.Controls.Add(this.start_search);
             this.Controls.Add(this.search_string);
             this.Name = "KvantuMarket";
             this.Text = "Kvantum Market";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_eld)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_mv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_el)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_ya)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_sb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +207,17 @@ namespace Kvantum_Market
 
         private System.Windows.Forms.TextBox search_string;
         private System.Windows.Forms.Button start_search;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox inf_eld;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox img_eld;
+        private System.Windows.Forms.TextBox inf_mv;
+        private System.Windows.Forms.PictureBox img_mv;
+        private System.Windows.Forms.TextBox inf_el;
+        private System.Windows.Forms.PictureBox img_el;
+        private System.Windows.Forms.TextBox inf_ya;
+        private System.Windows.Forms.PictureBox img_ya;
+        private System.Windows.Forms.PictureBox img_sb;
+        private System.Windows.Forms.TextBox inf_sb;
     }
 }
 
--- KvantumMarket/Kvantum Market/Form1.cs
+++ KvantumMarket/Kvantum Market/Form1.cs
@@ -1,5 +1,4 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +19,9 @@ namespace Kvantum_Market
         Dictionary<string, string> item_info_eld;
         Dictionary<string, string> item_info_mv;
         Dictionary<string, string> item_info_dns;
+        Dictionary<string, string> item_info_el;
+        Dictionary<string, string> item_info_ya;
+        Dictionary<string, string> item_info_sb;
 
         ChromeOptions options = new ChromeOptions();
         ChromeDriver driver;
@@ -38,19 +40,25 @@ namespace Kvantum_Market
             item_info_eld = new Dictionary<string, string>();
             item_info_mv = new Dictionary<string, string>();
             item_info_dns = new Dictionary<string, string>();
+            item_info_el = new Dictionary<string, string>();
+            item_info_ya = new Dictionary<string, string>();
+            item_info_sb = new Dictionary<string, string>();
             options.AddArguments("--headless");
             options.AddArguments("--disable-gpu");
             options.AddArguments("--remote-debugging-port=9222");
-            //options.AddArguments("--enable-javascript");
+            options.AddArguments("--enable-javascript");
             options.AddArguments("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'");
             options.AddArguments("--no-sandbox");
             options.AddArguments("--ignore-certificate-errors");
             options.AddArguments("--allow-insecure-localhost");
             options.AddArguments("--disable-blink-features=AutomationControlled");
-            driver = new ChromeDriver(@"D:\C#\KvantumMarket\KvantumMarket\Kvantum Market", options);
+            driver = new ChromeDriver(options);
             shop_url.Add("Эльдорадо", "https://www.eldorado.ru/search/catalog.php?q=");
             shop_url.Add("Днс", "https://www.dns-shop.ru/search/?q=");
             shop_url.Add("Мвидео", "https://www.mvideo.ru/product-list-page?q=");
+            shop_url.Add("Элекс", "https://elex.ru/search/?q=");
+            shop_url.Add("Яндекс", "https://market.yandex.ru/catalog--smartfony/54726/list?glfilter=12782797%3A25786890%2C25786910&text=");
+            shop_url.Add("Сбер", "https://sbermegamarket.ru/catalog/iphone-13-mini/#?related_search=");
 
             item_info_eld.Add("name", "//div/a[@data-dy='title']");
             item_info_eld.Add("price", "//div/span[@data-pc='offer_price']");
@@ -67,23 +75,46 @@ namespace Kvantum_Market
             item_info_dns.Add("img", "//div[@data-id='product']/div/a/picture/img");
             item_info_dns.Add("link", "//a[@class='catalog-product__name ui-link ui-link_black']");
 
+            item_info_el.Add("price", "//span[@class='product__new']");
+            item_info_el.Add("name", "//a[@class='product__name']");
+            item_info_el.Add("img", "//img[@itemprop='image']");
+            item_info_el.Add("link", "//div[@class='product__info']/a");
+
+            item_info_ya.Add("price", "//div[@data-zone-name='price']/div/a/div/span/span");
+            item_info_ya.Add("name", "//h3[@data-zone-name='title']");
+            item_info_ya.Add("img", "//div[@class='_2zfPd']/a/img");
+            item_info_ya.Add("link", "//div[@class='_2zfPd']/a");
+
+            item_info_sb.Add("price", "//div[@itemprop='offers']/span");
+            item_info_sb.Add("name", "//div[@itemprop='name']/a");
+            item_info_sb.Add("img", "//div[@class='item-image item-image_changeable']/a/img");
+            item_info_sb.Add("link", "//div[@class='item-image item-image_changeable']/a");
+
         }
 
         private void start_search_Click(object sender, EventArgs e)
         {
-            List<string> result = new List<string>();
+            Dictionary<string,string> result = new Dictionary<string, string>();
             string search = search_string.Text;
             result = get_data(shop_url["Эльдорадо"], search, item_info_eld);
-            foreach (string item in result)
-            {
-                textBox1.Text += item + Environment.NewLine;
-            }
+            img_eld.ImageLocation = $"{result["img"]}";
+            inf_eld.Text = $"{result["name"]}" + Environment.NewLine + $"{result["price"]}" ;
             result.Clear();
-            result = get_data(shop_url["Мвидео"], search, item_info_mv);
-            foreach (string item in result)
-            {
-                textBox1.Text += item + Environment.NewLine;
-            }
+            //result = get_data(shop_url["Мвидео"], search, item_info_mv);
+            //img_mv.ImageLocation = $"{result["img"]}";
+            //inf_mv.Text = $"{result["name"]}" + Environment.NewLine + $"{result["price"]}";
+            //result.Clear();
+            result = get_data(shop_url["Элекс"], search, item_info_el);
+            img_el.ImageLocation = $"{result["img"]}";
+            inf_el.Text = $"{result["name"]}" + Environment.NewLine + $"{result["price"]}";
+            result.Clear();
+            result = get_data(shop_url["Яндекс"], search, item_info_ya);
+            img_ya.ImageLocation = $"{result["img"]}";
+            inf_ya.Text = $"{result["name"]}" + Environment.NewLine + $"{result["price"]}";
+            result.Clear();
+            result = get_data(shop_url["Сбер"], search, item_info_sb);
+            img_sb.ImageLocation = $"{result["img"]}";
+            inf_sb.Text = $"{result["name"]}" + Environment.NewLine + $"{result["price"]}";
             result.Clear();
             //result = get_data(shop_url["Днс"], search, item_info_dns);
             //foreach (string item in result)
@@ -157,20 +188,20 @@ namespace Kvantum_Market
             #endregion
         }
 
-        private List<string> get_data(string url, string search, Dictionary<string,string> xpath) 
+        private Dictionary<string, string> get_data(string url, string search, Dictionary<string,string> xpath) 
         {
             driver.Navigate().GoToUrl($"{url}{search}");
-            Thread.Sleep(3000);
+            Thread.Sleep(1000);
             var name_item = driver.FindElement(By.XPath(xpath["name"]));
             var price_item = driver.FindElement(By.XPath(xpath["price"]));
             var img_item = driver.FindElement(By.XPath(xpath["img"]));
             var link_item = driver.FindElement(By.XPath(xpath["link"]));
-            List<string> result = new List<string>
+            Dictionary<string, string> result = new Dictionary<string, string>
             {
-                name_item.Text,
-                price_item.Text,
-                img_item.GetAttribute("src"),
-                link_item.GetAttribute("href")
+                ["name"] = name_item.Text,
+                ["price"] = price_item.Text,
+                ["img"] = img_item.GetAttribute("src"),
+                ["link"] = link_item.GetAttribute("href")
 
             };
 
